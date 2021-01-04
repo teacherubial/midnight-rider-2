bb3d2b06310c849727c3c5d7d64939e4d9f1ec34
Tim Ubial <tubial@sd38.bc.ca>
2021-01-04 11:31:58 -0800

    added status check and did some usability testing

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ REACH THE END BEFORE THE MAN GON GETCHU.
 
 CHOICES = """
     ----
+    E. Status check
     Q. QUIT
     ----
 """
@@ -41,9 +42,18 @@ def intro():
 
 
 def main():
-    intro()
+    # intro()
 
+    # Variables
     done = False
+
+    km_traveled = 0             # 100km traveled is the goal
+    agents_distance = -20.0
+    turns = 0                   # amount of turns taken
+    tofu = 3                    # 3 is max tofu
+    fuel = 50                   # 50 is a full tank
+    hunger = 0                  # hunger increases with num
+
 
     while not done:
         # TODO: Check if reached END GAME
@@ -53,8 +63,19 @@ def main():
 
         # Handle user's input
         users_choice = input("What do you want to do? ").lower().strip("!,.? ")
-        if users_choice == "q":
+
+        if users_choice == "e":
+            print(f"\t---Status Check---")
+            print(f"\tkms traveled: {km_traveled} kms")
+            print(f"\tFuel left: {fuel} L")
+            print(f"\tAgents are {abs(agents_distance)} kms behind you.")
+            print(f"\tYou have {tofu} tofu left.")
+            print("\t------\n")
+        elif users_choice == "q":
             done = True
+
+        # Pause
+        time.sleep(1)
 
         # TODO: Change the environment based on choice and RNG
 
