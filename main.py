8d72594adf36720641b0fde8cb37b18855a1bf14
Tim Ubial <tubial@sd38.bc.ca>
2021-01-04 10:24:47 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# main.py
+# Midnight Rider
+# A text adventure game that is riveting.
+# IGN gives it 4 stars out of 100.
+
+
+def main():
+    pass
+
+    # TODO: Show introduction
+
+    # LOOP
+    # TODO: Check if reached END GAME
+
+    # TODO: Give the player their choices
+
+    # TODO: Change the environment based on choice and RNG
+
+
+if __name__ == '__main__':
+    main()
