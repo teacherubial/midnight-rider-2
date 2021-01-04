66536c175b71bb0ea94125a3ce0fff52a350dfb8
Tim Ubial <tubial@sd38.bc.ca>
2021-01-04 10:45:32 -0800

    implemented choice and quit

--- main.py
+++ main.py
@@ -3,18 +3,63 @@
 # A text adventure game that is riveting.
 # IGN gives it 4 stars out of 100.
 
+import sys
+import textwrap
+import time
+
+
+INTRODUCTION = """
+
+WELCOME TO MIDNIGHT RIDER
+
+WE'VE STOLEN A CAR. WE NEED TO GET IT HOME.
+THE CAR IS SPECIAL.
+
+THE GOVERNMENT WANTS IT FOR THEIR GAIN.
+
+WE CAN'T LET THAT HAPPEN.
+
+ONE GOAL: SURVIVAL... and THE CAR.
+REACH THE END BEFORE THE MAN GON GETCHU.
+
+"""
+
+CHOICES = """
+    ----
+    Q. QUIT
+    ----
+"""
+
+
+def intro():
+    for char in textwrap.dedent(INTRODUCTION):
+        time.sleep(0.05)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+    time.sleep(1)
+
 
 def main():
-    pass
+    intro()
 
-    # TODO: Show introduction
+    done = False
 
-    # LOOP
-    # TODO: Check if reached END GAME
+    while not done:
+        # TODO: Check if reached END GAME
 
-    # TODO: Give the player their choices
+        # Give the player their choices
+        print(CHOICES)
 
-    # TODO: Change the environment based on choice and RNG
+        # Handle user's input
+        users_choice = input("What do you want to do? ").lower().strip("!,.? ")
+        if users_choice == "q":
+            done = True
+
+        # TODO: Change the environment based on choice and RNG
+
+    # Outroduction
+    print("Thanks for playing! Please play again. =)")
 
 
 if __name__ == '__main__':
